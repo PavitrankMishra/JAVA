cc406c400a9c7e09fb9f1b127761a5c78db95b3a
Pavitrank <pavitrank2002@gmail.com>
2024-08-02 17:24:24 +0530

    feat: Reverse a linkedlist

--- src/com/company/LinkedList/ll1.java
+++ src/com/company/LinkedList/ll1.java
@@ -98,6 +98,29 @@ public class ll1 {
         int getSize() {
             return size;
         }
+
+        Node getNodeAt(int idx) {
+            Node temp = head;
+            for(int i=0;i<idx;i++) {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        void reverseDI() {
+            int li = 0;
+            int ri = size-1;
+            while(li<ri) {
+                Node left = getNodeAt(li);
+                Node right = getNodeAt(ri);
+
+                int temp = left.data;
+                left.data = right.data;
+                right.data = temp;
+
+                li++;
+                ri--;
+            }
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/com/company/LinkedList/ll2.java
@@ -0,0 +1,20 @@
+package com.company.LinkedList;
+
+//Reverse a Linked List
+
+public class ll2 extends ll1{
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+
+        list.show();
+
+        list.reverseDI();
+
+        list.show();
+    }
+}
